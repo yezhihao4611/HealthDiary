631f2a3b8b7deeb0302aac4e25c6133c14d23250
7hens <thenstango@live.com>
2016-10-20 16:11:17 +0800

    20161020 Hao

---
+++ app/src/main/java/com/tz/healthdiary/utils/CharterActivity.java
@@ -0,0 +1,20 @@
+package com.tz.healthdiary.utils;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Administrator on 2016/10/20 0020.
+ */
+
+public class CharterActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
