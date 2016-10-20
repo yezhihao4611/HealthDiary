d5acd5d0823d3d1cdc93926c4bb1cdb498487334
anzhuo <673429405@qq.com>
2016-10-20 16:16:50 +0800

    20161020 LT

---
+++ app/src/main/java/com/tz/healthdiary/WaistlineActivity.java
@@ -0,0 +1,55 @@
+package com.tz.healthdiary;
+
+import android.app.Activity;
+import android.icu.math.BigDecimal;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import static com.tz.healthdiary.R.id.rb_wumen_waist;
+
+/**
+ * Created by anzhuo on 2016/10/20.
+ */
+
+public class WaistlineActivity extends Activity implements View.OnClickListener {
+    EditText et_year_waist;
+    RadioButton rb_man_waise;
+    RadioButton rb_wumen_waise;
+    EditText et_height_waist;
+    EditText et_waist_waist;
+    LinearLayout ll_result_waist;
+    TextView tv_result_waist;
+    int height;
+    int waist;
+    double resulr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.other_waistline_layout);
+        et_year_waist = (EditText) findViewById(R.id.et_year_waist);
+        rb_man_waise = (RadioButton) findViewById(R.id.rb_man_waist);
+        rb_wumen_waise = (RadioButton) findViewById(rb_wumen_waist);
+        et_height_waist = (EditText) findViewById(R.id.et_height_waist);
+        et_waist_waist = (EditText) findViewById(R.id.et_waist_waist);
+        ll_result_waist = (LinearLayout) findViewById(R.id.ll_result_waist);
+        tv_result_waist = (TextView) findViewById(R.id.tv_result_waist);
+        height = Integer.parseInt(et_height_waist.getText().toString());
+        waist=Integer.parseInt(et_waist_waist.getText().toString());
+        resulr=(double) waist/height;
+        String str=String.format("%.2f",resulr);
+        if (height >= 20 && height <= 240&&waist>=20&&waist<=150){
+            tv_result_waist.setText(str);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
--- app/src/main/java/com/tz/healthdiary/fragment/OtherFragment.java
+++ app/src/main/java/com/tz/healthdiary/fragment/OtherFragment.java
@@ -1,27 +1,60 @@
 package com.tz.healthdiary.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.tz.healthdiary.BaseActivity;
+import com.tz.healthdiary.BodyActivity;
 import com.tz.healthdiary.R;
+import com.tz.healthdiary.WaistlineActivity;
 
 /**
  * Created by 西野七濑 on 2016/10/18.
  * Other页面的Fragment
  */
-public class OtherFragment extends Fragment {
+public class OtherFragment extends Fragment implements View.OnClickListener {
+    TextView tv_waist_other;
+    TextView tv_body_other;
+    TextView tv_base_other;
+    Intent intent;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_test,null);
-        return null;
+        View view = inflater.inflate(R.layout.oter_layout,null);
+        tv_waist_other= (TextView) view.findViewById(R.id.tv_waist_other);
+        tv_body_other= (TextView) view.findViewById(R.id.tv_body_other);
+        tv_base_other= (TextView) view.findViewById(R.id.tv_base_other);
+        tv_waist_other.setOnClickListener(this);
+        tv_body_other.setOnClickListener(this);
+        tv_base_other.setOnClickListener(this);
+        return view;
     }
+
+
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_waist_other:
+                intent=new Intent(getActivity(), WaistlineActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tv_body_other:
+                intent=new Intent(getActivity(), BodyActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.tv_base_other:
+                intent=new Intent(getActivity(), BaseActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+
+
     }
 }
