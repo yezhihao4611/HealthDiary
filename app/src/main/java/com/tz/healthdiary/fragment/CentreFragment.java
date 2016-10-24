a6adf040f682c6707d1036d66ae2a3960d2fca55
7hens <thenstango@live.com>
2016-10-24 16:01:38 +0800

    20161024 Hao

--- app/src/main/java/com/tz/healthdiary/fragment/CentreFragment.java
+++ app/src/main/java/com/tz/healthdiary/fragment/CentreFragment.java
@@ -1,32 +1,7 @@
 package com.tz.healthdiary.fragment;
 
 
-import android.graphics.Color;
-import android.graphics.PointF;
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-
-import android.support.v4.content.ContextCompat;
-import android.util.DisplayMetrics;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-
-import com.idtk.smallchart.chart.CurveChart;
-import com.idtk.smallchart.chart.LineChart;
-import com.idtk.smallchart.data.CurveData;
-import com.idtk.smallchart.data.LineData;
-import com.idtk.smallchart.data.PointShape;
-import com.idtk.smallchart.interfaces.iData.ICurveData;
-import com.idtk.smallchart.interfaces.iData.ILineData;
-import com.tz.healthdiary.R;
-
-import java.util.ArrayList;
-
+import android.app.Fragment;
 
 /**
  * Created by 西野七濑 on 2016/10/18.
@@ -35,41 +10,5 @@ import java.util.ArrayList;
 
 public class CentreFragment extends Fragment {
 
-    private LineData mLineData = new LineData();
-    private ArrayList<ILineData> mDataList = new ArrayList<>();
-    private ArrayList<PointF> mPointArrayList = new ArrayList<>();
-    protected float[][] points = new float[][]{{1, 1}, {2, 10}, {3, 4}, {4, 8}, {5, 12},{6, 12},{7, 13},{8, 8}};
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_center, null);
-
-        mPointArrayList.clear();
-        initData();
-        LineChart lineChart = (LineChart) view.findViewById(R.id.lineChart);
-//        lineChart.setData(mLineData);
-        lineChart.setDataList(mDataList);
-
-        return view;
-    }
-
-    private void initData() {
-        for (int i = 0; i < 8; i++) {
-            mPointArrayList.add(new PointF(points[i][0], points[i][1]));
-        }
-        mLineData.setValue(mPointArrayList);
-        mLineData.setColor(Color.MAGENTA);
-        mLineData.setPaintWidth(pxTodp(3));
-        mLineData.setTextSize(pxTodp(10));
-        mDataList.add(mLineData);
-    }
-
-    protected float pxTodp(float value) {
-        DisplayMetrics metrics = new DisplayMetrics();
-        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
-        float valueDP = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, value, metrics);
-        return valueDP;
-    }
 
 }
