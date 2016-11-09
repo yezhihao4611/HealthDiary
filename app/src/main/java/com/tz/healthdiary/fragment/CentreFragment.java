33333526c80be195b3d99ea090229333e488a94d
7hens <thenstango@live.com>
2016-11-09 15:10:01 +0800

    20161107 Hao

--- app/src/main/java/com/tz/healthdiary/fragment/CentreFragment.java
+++ app/src/main/java/com/tz/healthdiary/fragment/CentreFragment.java
@@ -50,7 +50,6 @@ public class CentreFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_center, null);
-
         return view;
     }
 
